fa183057c10dd39405fea3facc609f30cf7af7e3
Sébastien Chèvre <seb.chevre@gmail.com>
2018-10-01 08:22:27 +0200

    ok

---
+++ simple-kafka-consommateur/src/main/java/ch/globaz/devsecops/kafka/consommateur/producteur/MessageProducteur.java
@@ -0,0 +1,24 @@
+package ch.globaz.devsecops.kafka.producteur.producteur;
+
+import ch.globaz.devsecops.kafka.common.HelloWorld;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+
+
+public class MessageProducteur {
+
+    @Autowired
+    private KafkaTemplate<String, HelloWorld> kafkaTemplate;
+
+
+    @Value(value = "${test.topic.name}")
+    private String topicName;
+
+
+    public void sendMessage(HelloWorld message) {
+
+        kafkaTemplate.send(topicName, message);
+    }
+
+}
